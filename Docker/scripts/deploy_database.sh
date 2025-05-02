e920126536f596cb9fdf3956677c7b779d415942
Wanderson Chaves <wandersonchavesbr@gmail.com>
2025-05-02 10:58:42 -0300

    fix(docker): add DATABASE_CONNECTION_URI

--- Docker/scripts/deploy_database.sh
+++ Docker/scripts/deploy_database.sh
@@ -7,9 +7,9 @@ if [ "$DOCKER_ENV" != "true" ]; then
 fi
 
 if [[ "$DATABASE_PROVIDER" == "postgresql" || "$DATABASE_PROVIDER" == "mysql" ]]; then
-    export DATABASE_URL
+    export DATABASE_CONNECTION_URI
     echo "Deploying migrations for $DATABASE_PROVIDER"
-    echo "Database URL: $DATABASE_URL"
+    echo "Database URL: $DATABASE_CONNECTION_URI"
     # rm -rf ./prisma/migrations
     # cp -r ./prisma/$DATABASE_PROVIDER-migrations ./prisma/migrations
     npm run db:deploy

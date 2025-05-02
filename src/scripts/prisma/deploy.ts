9333e566623acd7098ba70cd9152eeb62c9778c3
Wanderson Chaves <wandersonchavesbr@gmail.com>
2025-05-02 09:36:32 -0300

    fix(docker/prisma): move scripts para raiz e corrige path relativo para execução com ts-node

--- src/scripts/prisma/deploy.ts
+++
@@ -1,8 +0,0 @@
-import {execSync} from 'child_process'
-import dotenv from 'dotenv'
-
-dotenv.config()
-execSync(
-  `npx prisma migrate deploy --schema prisma/${process.env.DATABASE_PROVIDER}-schema.prisma`,
-  {stdio: 'inherit'},
-)
--- src/scripts/prisma/generate.ts
+++
@@ -1,5 +0,0 @@
-// scripts/prisma/generate.ts
-import {execSync} from 'node:child_process'
-
-console.log('🧬 Running Prisma Generate...')
-execSync('npx prisma generate', {stdio: 'inherit'})
